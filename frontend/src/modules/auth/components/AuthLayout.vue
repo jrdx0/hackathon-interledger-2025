4da6f955dc82d73a3a48d355e3efec14ba232028
Jose Jair Medrano Olmos <60531536+JairC137@users.noreply.github.com>
2025-11-09 00:20:25 -0600

    login

---
+++ frontend/src/modules/auth/components/AuthLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="layout">
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #f3f4f6;
+}
+</style>
